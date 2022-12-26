21e05ea3db0a518632219063d29810043ff82771
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-12-26 12:39:07 +0100

    feat: routed links

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import Directory from './components/Directory/Directory';
+import { Route, Routes } from 'react-router-dom';
+import Home from './routes/home/Home';
+import Navigation from './routes/navigation/Navigation';
+import SignIn from './routes/signIn/SignIn';
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
 
 const App = () => {
-  return <Directory />;
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
