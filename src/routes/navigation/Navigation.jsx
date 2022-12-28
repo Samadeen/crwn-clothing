ac351014307e927208023dbef9de59546f26217c
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-12-28 16:29:29 +0100

    feat: added sign out button

--- src/routes/navigation/Navigation.jsx
+++ src/routes/navigation/Navigation.jsx
@@ -1,8 +1,14 @@
 import { Link, NavLink, Outlet } from 'react-router-dom';
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
 import './Navigation.scss';
+import { useContext } from 'react';
+import { UserContext } from '../../contexts/UserContext';
+import { signOutUser } from '../../utils/Firebase/Firebase';
 
 const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+  console.log(currentUser);
+
   return (
     <>
       <div className='navigation'>
@@ -13,9 +19,15 @@ const Navigation = () => {
           <NavLink to='/shop' className='nav-link'>
             SHOP
           </NavLink>
-          <NavLink className='nav-link' to='/auth'>
-            SIGN IN
-          </NavLink>
+          {currentUser ? (
+            <span className='nav-link' onClick={signOutUser}>
+              SIGN OUT
+            </span>
+          ) : (
+            <NavLink className='nav-link' to='/auth'>
+              SIGN IN
+            </NavLink>
+          )}
         </nav>
       </div>
       <Outlet />
