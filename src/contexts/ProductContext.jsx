38b09675fe0550656891bdc7ff18d80439b6bffb
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-01-01 18:07:45 +0100

    feat: added product data

--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState } from 'react';
 
-import PRODUCTS from '../shop-data.json';
+import PRODUCTS from '../data/shop-data.json';
 
 export const ProductContext = createContext({
   product: [],
