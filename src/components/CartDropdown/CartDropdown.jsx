66a5a238bfe80bcbebf2f52d9e310382d3044f5b
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-01-04 18:25:11 +0100

    Update CartDropdown.jsx

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -1,10 +1,18 @@
+import { useContext } from 'react';
 import Button from '../Button/Button';
+import CartItem from '../CartItem/CartItem';
 import './CartDropdown.scss';
+import { CartContext } from '../../contexts/CartContext';
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
   return (
     <div className='cart-dropdown-container'>
-      <div className='cart-items' />
+      <div className='cart-items'>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
       <Button>CHECKOUT</Button>
     </div>
   );
