05a3f0cefe43106216dbd7720a6f98388f3a217a
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-01-04 18:25:00 +0100

    Update ProductCard.jsx

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './ProductCard.scss';
 import Button from '../Button/Button';
+import { CartContext } from '../../contexts/CartContext';
 
 const ProductCard = ({ products }) => {
   const { name, price, imageUrl } = products;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCartHandler = () => addItemToCart(products);
 
   return (
     <div className='product-card-container'>
@@ -12,7 +16,9 @@ const ProductCard = ({ products }) => {
         <span className='name'>{name}</span>
         <span className='price'>${price}</span>
       </div>
-      <Button buttonType='inverted'>Add to Cart</Button>
+      <Button buttonType='inverted' onClick={addProductToCartHandler}>
+        Add to Cart
+      </Button>
     </div>
   );
 };
