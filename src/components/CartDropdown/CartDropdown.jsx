dd5f70e2a9b7ca04ba415b97a322a4d9a28165df
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-01-15 00:39:27 +0100

    feat: added goto checkout handler

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -3,9 +3,16 @@ import Button from '../Button/Button';
 import CartItem from '../CartItem/CartItem';
 import './CartDropdown.scss';
 import { CartContext } from '../../contexts/CartContext';
+import { useNavigate } from 'react-router-dom';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const gotoCheckoutHandler = () => {
+    navigate('/checkout');
+  };
+
   return (
     <div className='cart-dropdown-container'>
       <div className='cart-items'>
@@ -13,7 +20,7 @@ const CartDropdown = () => {
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button>CHECKOUT</Button>
+      <Button onClick={gotoCheckoutHandler}>CHECKOUT</Button>
     </div>
   );
 };
