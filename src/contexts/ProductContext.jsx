600b791d2d5eaa11b713ffa9760b0b0842e82cd3
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-01-28 20:14:50 +0100

    Update ProductContext.jsx

--- src/contexts/ProductContext.jsx
+++ src/contexts/ProductContext.jsx
@@ -1,13 +1,13 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
-import PRODUCTS from '../data/shop-data.json';
+import { addCollectionAndDocuments } from '../utils/Firebase/Firebase';
 
 export const ProductContext = createContext({
   product: [],
 });
 
 export const ProductProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
   const value = { products };
   return (
     <ProductContext.Provider value={value}>{children}</ProductContext.Provider>
